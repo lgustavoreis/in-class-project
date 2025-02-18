dc10a0df89c7946414580921b30e2d6f77010836
Gustavo <luiz-gustav@hotmail.com>
2025-02-18 15:25:22 -0500

    trying to improve

---
+++ in-class-project/app/index.js
@@ -0,0 +1,4 @@
+import './index.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+console.log('Frontend loaded');
---
+++ in-class-project/app/webpack.coonfig.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './index.js', // Corrija para apontar para o arquivo correto
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    }),
+    new MiniCssExtractPlugin({ filename: 'styles.css' })
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 8080
+  }
+};
